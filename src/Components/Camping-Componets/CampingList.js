e04cef1458867cf89dd5addcc802760a312bd3c3
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2020-04-21 18:44:56 -0700

    Update CampingList.js

--- src/Components/Camping-Componets/CampingList.js
+++ src/Components/Camping-Componets/CampingList.js
@@ -140,7 +140,7 @@ class CampingList extends React.Component {
         console.log(this.state.tent)
         return(
             <main>
-                <button className="list-home" onClick={() => { window.location.assign('http://localhost:3000/') }}>Home</button>
+                <button className="list-home" onClick={() => { window.location.assign('https://outdoorgearpicker.herokuapp.com/') }}>Home</button>
                <table className="list-table">
                    <tr className="list-tr">
                        <th className="list-th">Gear Type</th>
@@ -151,7 +151,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">tent</td>
                         {this.state.tent.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.tentImage} /></td> : <td></td>}
-                        {this.state.tent.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=tent'>Add tent</a> : <td className="list-td">{this.state.tent}</td>}
+                        {this.state.tent.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=tent'>Add tent</a> : <td className="list-td">{this.state.tent}</td>}
                         {this.state.tent.length === 0 ? <td></td> : <td className="list-td">${this.state.tentPrice}</td>}
                         {this.state.tent.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.tl}>Buy Now!</button></td> : <td></td>}
                         {this.state.tent.length !== 0 ? <button className="list-delete" value="tent" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -159,7 +159,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">bedding</td>
                         {this.state.bedding.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.beddingImage} /></td> : <td></td>}
-                        {this.state.bedding.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=bedding'>Add bedding</a> : <td className="list-td">{this.state.bedding}</td>}
+                        {this.state.bedding.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=bedding'>Add bedding</a> : <td className="list-td">{this.state.bedding}</td>}
                         {this.state.bedding.length === 0 ? <td></td> : <td className="list-td">${this.state.beddingPrice}</td>}
                         {this.state.bedding.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.bl}>Buy Now!</button></td> : <td></td>}
                         {this.state.bedding.length !== 0 ? <button className="list-delete" value="bedding" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -167,7 +167,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">cookingstove</td>
                         {this.state.cookingstove.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.cookingstoveImage} /></td> : <td></td>}
-                        {this.state.cookingstove.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=cookingstove'>Add cookingstove</a> : <td className="list-td">{this.state.cookingstove}</td>}
+                        {this.state.cookingstove.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=cookingstove'>Add cookingstove</a> : <td className="list-td">{this.state.cookingstove}</td>}
                         {this.state.cookingstove.length === 0 ? <td></td> : <td className="list-td">${this.state.cookingstovePrice}</td>}
                         {this.state.cookingstove.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.csl}>Buy Now!</button></td> : <td></td>}
                         {this.state.cookingstove.length !== 0 ? <button className="list-delete" value="cookingstove" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -175,7 +175,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">lantern</td>
                         {this.state.lantern.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.lanternImage} /></td> : <td></td>}
-                        {this.state.lantern.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=lantern'>Add lantern</a> : <td className="list-td">{this.state.lantern}</td>}
+                        {this.state.lantern.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=lantern'>Add lantern</a> : <td className="list-td">{this.state.lantern}</td>}
                         {this.state.lantern.length === 0 ? <td></td> : <td className="list-td">${this.state.lanternPrice}</td>}
                         {this.state.lantern.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.ll}>Buy Now!</button></td> : <td></td>}
                         {this.state.lantern.length !== 0 ? <button className="list-delete" value="lantern" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -183,7 +183,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">firstaid</td>
                         {this.state.firstaid.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.firstaidImage} /></td> : <td></td>}
-                        {this.state.firstaid.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=firstaid'>Add firstaid</a> : <td className="list-td">{this.state.firstaid}</td>}
+                        {this.state.firstaid.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=firstaid'>Add firstaid</a> : <td className="list-td">{this.state.firstaid}</td>}
                         {this.state.firstaid.length === 0 ? <td></td> : <td className="list-td">${this.state.firstaidPrice}</td>}
                         {this.state.firstaid.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.fal}>Buy Now!</button></td> : <td></td>}
                         {this.state.firstaid.length !== 0 ? <button className="list-delete" value="firstaid" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -191,7 +191,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">firestarter</td>
                         {this.state.firestarter.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.firestarterImage} /></td> : <td></td>}
-                        {this.state.firestarter.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=firestarter'>Add firestarter</a> : <td className="list-td">{this.state.firestarter}</td>}
+                        {this.state.firestarter.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=firestarter'>Add firestarter</a> : <td className="list-td">{this.state.firestarter}</td>}
                         {this.state.firestarter.length === 0 ? <td></td> : <td className="list-td">${this.state.firestarterPrice}</td>}
                         {this.state.firestarter.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.fsl}>Buy Now!</button></td> : <td></td>}
                         {this.state.firestarter.length !== 0 ? <button className="list-delete" value="firestarter" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -199,7 +199,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">Water Container</td>
                         {this.state.watercontainer.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.watercontainerImage} /></td> : <td></td>}
-                        {this.state.watercontainer.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=watercontainer'>Add Water Container</a> : <td className="list-td">{this.state.watercontainer}</td>}
+                        {this.state.watercontainer.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=watercontainer'>Add Water Container</a> : <td className="list-td">{this.state.watercontainer}</td>}
                         {this.state.watercontainer.length === 0 ? <td></td> : <td className="list-td">${this.state.watercontainerPrice}</td>}
                         {this.state.watercontainer.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.wcl}>Buy Now!</button></td> : <td></td>}
                         {this.state.watercontainer.length !== 0 ? <button className="list-delete" value="watercontainer" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
@@ -207,7 +207,7 @@ class CampingList extends React.Component {
                    <tr className="list-tr">
                         <td className="list-td">Water Filter</td>
                         {this.state.waterfilter.length !== 0 ? <td className="list-td"><img className="list-image" src={this.state.waterfilterImage} /></td> : <td></td>}
-                        {this.state.waterfilter.length === 0 ? <a className="newlist-a" href='http://localhost:3000/editcampingproductsearch?type=waterfilter'>Add Water Filter</a> : <td className="list-td">{this.state.waterfilter}</td>}
+                        {this.state.waterfilter.length === 0 ? <a className="newlist-a" href='https://outdoorgearpicker.herokuapp.com/editcampingproductsearch?type=waterfilter'>Add Water Filter</a> : <td className="list-td">{this.state.waterfilter}</td>}
                         {this.state.waterfilter.length === 0 ? <td></td> : <td className="list-td">${this.state.waterfilterPrice}</td>}
                         {this.state.waterfilter.length !== 0 ? <td className="list-td"><button className="buybutton" onClick={this.buyNow.bind(this)} value={this.state.wfl}>Buy Now!</button></td> : <td></td>}
                         {this.state.waterfilter.length !== 0 ? <button className="list-delete" value="waterfilter" onClick={this.handleDelete.bind(this)}>X</button> : <td></td>}
